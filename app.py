ed941bd160fb3f4cf8d55aef9da22e08f7caf022
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-09-13 14:15:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,Response,jsonify, render_template ,logging,request
+from flask import Flask, Response, jsonify, render_template, logging, request
 
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
